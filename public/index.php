354d771f6fbed227e6b4cb82820130991139a719
mosama88 <mosama88@hotmail>
2024-11-06 12:20:01 +0200

    Done Go

--- public/index.php
+++ public/index.php
@@ -4,13 +4,11 @@
 // ini_set('memory_limit', '255M');
 // echo ini_get('memory_limit');
 
-echo "<pre>";
-print_r($_SERVER);
-echo "</pre>";
+
+
+include __DIR__ . "/../src/App/function.php";
 
 $app  = include __DIR__  . "/../src/App/bootstrap.php";
 
-
-
-
 $app->run();
+dd($app);
--- src/Framework/App.php
+++ src/Framework/App.php
@@ -8,6 +8,16 @@ namespace Framework;
 
 class App
 {
+
+
+    private Router $router;
+
+    public function __construct(){
+        $this->router = new Router();
+    }
+
+
+    
     public function run()
     {
         echo "Welcome tom My Application";
